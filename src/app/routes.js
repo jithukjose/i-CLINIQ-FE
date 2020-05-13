ba6254acfdeee52771c615accef99433f97e368e
Jithuk123 <jithujose@experionglobal.com>
2020-05-13 13:04:32 +0530

    appointment module UI created

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,17 +1,18 @@
-import Dashboard from './Containers/Dashboard';
-import Notifications from './Containers/Notifications.jsx';
-import Icons from './Containers/Icons';
-import Typography from './Containers/Typography.jsx';
-import TableList from './Containers/Tables';
+// import Dashboard from './Containers/Dashboard';
+// import Notifications from './Containers/Notifications.jsx';
+// import Icons from './Containers/Icons';
+// import Typography from './Containers/Typography.jsx';
+// import TableList from './Containers/Tables';
 // import Maps from './views/Map.jsx';
-import UserPage from './Containers/User.jsx';
-import UpgradeToPro from './Containers/Upgrade.jsx';
+// import UserPage from './Containers/User.jsx';
+// import UpgradeToPro from './Containers/Upgrade.jsx';
 import MyDashboard from './Container/Dashboard';
-import DoctorContainer from './Container/Doctor';
-import LabTechnicianContainer from './Container/LabTechnician';
-import MedicineContainer from './Container/Medicine';
-import MedicalTestContainer from './Container/MedicalTests';
+import Doctor from './Container/Doctor';
+import LabTechnician from './Container/LabTechnician';
+import Medicine from './Container/Medicine';
+import MedicalTest from './Container/MedicalTests';
 import Profile from './Container/Profile';
+import Appointments from './Container/Appointments';
 
 var routes = [
   {
@@ -25,28 +26,35 @@ var routes = [
     path: '/doctor',
     name: 'DOCTOR',
     icon: 'fas fa-user-md',
-    component: DoctorContainer,
+    component: Doctor,
     layout: '/admin',
   },
   {
     path: '/lab-technician',
     name: 'LAB TECHNICIAN',
     icon: 'fas fa-microscope',
-    component: LabTechnicianContainer,
+    component: LabTechnician,
     layout: '/admin',
   },
   {
     path: '/medicine',
     name: 'MEDICINES',
     icon: 'fas fa-pills',
-    component: MedicineContainer,
+    component: Medicine,
     layout: '/admin',
   },
   {
     path: '/lab-tests',
     name: 'LAB TESTS',
     icon: 'fas fa-vials',
-    component: MedicalTestContainer,
+    component: MedicalTest,
+    layout: '/admin',
+  },
+  {
+    path: '/appointments',
+    name: 'APPOINTMENTS',
+    icon: 'fas fa-newspaper',
+    component: Appointments,
     layout: '/admin',
   },
   {
@@ -56,63 +64,59 @@ var routes = [
     component: Profile,
     layout: '/admin',
   },
-  {
-    path: '/user-page',
-    name: 'Profile',
-    icon: 'nc-icon nc-single-02',
-    component: UserPage,
-    layout: '/admin',
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    icon: 'nc-icon nc-bank',
-    component: Dashboard,
-    layout: '/admin',
-  },
 
-  {
-    path: '/notifications',
-    name: 'Notifications',
-    icon: 'nc-icon nc-bell-55',
-    component: Notifications,
-    layout: '/admin',
-  },
+  // {
+  //   path: '/user-page',
+  //   name: 'Profile',
+  //   icon: 'nc-icon nc-single-02',
+  //   component: UserPage,
+  //   layout: '/admin',
+  // },
+  // {
+  //   path: '/dashboard',
+  //   name: 'Dashboard',
+  //   icon: 'nc-icon nc-bank',
+  //   component: Dashboard,
+  //   layout: '/admin',
+  // },
 
-  {
-    path: '/tables',
-    name: 'Table List',
-    icon: 'nc-icon nc-tile-56',
-    component: TableList,
-    layout: '/admin',
-  },
+  // {
+  //   path: '/notifications',
+  //   name: 'Notifications',
+  //   icon: 'nc-icon nc-bell-55',
+  //   component: Notifications,
+  //   layout: '/admin',
+  // },
 
-  {
-    path: '/typography',
-    name: 'Typography',
-    icon: 'nc-icon nc-caps-small',
-    component: Typography,
-    layout: '/admin',
-  },
-  {
-    path: '/icons',
-    name: 'Icons',
-    icon: 'nc-icon nc-diamond',
-    component: Icons,
-    layout: '/admin',
-  },
+  // {
+  //   path: '/tables',
+  //   name: 'Table List',
+  //   icon: 'nc-icon nc-tile-56',
+  //   component: TableList,
+  //   layout: '/admin',
+  // },
 
-  {
-    pro: true,
-    path: '/upgrade',
-    name: 'Upgrade to PRO',
-    icon: 'nc-icon nc-spaceship',
-    component: UpgradeToPro,
-    layout: '/admin',
-  },
-  {
-    name: 'APPOINTMENTS',
-    icon: 'fas fa-newspaper',
-  },
+  // {
+  //   path: '/typography',
+  //   name: 'Typography',
+  //   icon: 'nc-icon nc-caps-small',
+  //   component: Typography,
+  //   layout: '/admin',
+  // },
+  // {
+  //   path: '/icons',
+  //   name: 'Icons',
+  //   icon: 'nc-icon nc-diamond',
+  //   component: Icons,
+  //   layout: '/admin',
+  // },
+
+  // {
+  //   path: '/upgrade',
+  //   name: 'Upgrade to PRO',
+  //   icon: 'nc-icon nc-spaceship',
+  //   component: UpgradeToPro,
+  //   layout: '/admin',
+  // },
 ];
 export default routes;
